157c8dcb2ef42ee59c3fb30e3bf290db72b8e534
ex-hezhihu870 <ex-hezhihu870@pingan.com.cn>
2025-02-12 12:11:10 +0800

    下载管理页面添加功能

--- lib/page/download/logic.dart
+++ lib/page/download/logic.dart
@@ -1,7 +1,9 @@
+import 'package:app_installer/app_installer.dart';
 import 'package:gstore/core/core.dart';
 import 'package:gstore/db/apps/AppInfoDatabase.dart';
 import 'package:gstore/http/download/DownloadStatus.dart';
 import 'package:gstore/http/download/DownloadStatusDataBase.dart';
+import 'package:gstore/http/download/downloadService.dart';
 
 import 'state.dart';
 
@@ -33,6 +35,33 @@ class DownloadManagerLogic extends GetxController with GithubRequestMix {
 
   Future<AppInfo?> getAppInfo(String appId) async {
     return (await appInfoDB).dao.getAppInfo(appId);
+  }
+
+  void installApp(DownloadStatus downStatus) {
+    // 实现安装应用的逻辑
+    if (GetPlatform.isAndroid && downStatus.fileName.endsWith(".apk")) {
+      AppInstaller.installApk(downStatus.savePath);
+    }
+  }
+
+  void retryDownload(DownloadStatus downStatus) async {
+    // 实现重新下载的逻辑
+    downStatus.updateDownload(0, downStatus.total);
+    await Get.find<DownloadService>().download(
+        downStatus.appId,
+        downStatus.appName,
+        downStatus.version,
+        downStatus.downloadUrl,
+        downStatus.fileName,
+        downloadSize: downStatus.total);
+  }
+
+  void pauseDownload(DownloadStatus downStatus) {
+    // 实现暂停下载的逻辑
+  }
+
+  void deleteDownload(DownloadStatus downStatus) async {
+    // 实现删除下载的逻辑
   }
 
   @override
