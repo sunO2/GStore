ccb9941ef7d4c593bd8a07e08cb145b580980e92
hezhihu89 <hezhihu89@gmail.com>
2025-02-22 11:50:17 +0900

    添加动画

--- lib/page/update/view.dart
+++ lib/page/update/view.dart
@@ -1,4 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gstore/core/routers.dart';
@@ -12,6 +13,33 @@ class UpdateManager extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+    var colors = <String,Color>{
+      "primary":colorScheme.primary,
+      "primaryContainer":colorScheme.primaryContainer,
+      "primaryFixed":colorScheme.primaryFixed,
+      "primaryFixedDim":colorScheme.primaryFixedDim,
+      "secondary":colorScheme.secondary,
+      "secondaryContainer":colorScheme.secondaryContainer,
+      "secondaryFixed":colorScheme.secondaryFixed,
+      "secondaryFixedDim":colorScheme.secondaryFixedDim,
+      "tertiary":colorScheme.tertiary,
+      "tertiaryContainer":colorScheme.tertiaryContainer,
+      "tertiaryFixed":colorScheme.tertiaryFixed,
+      "tertiaryFixedDim":colorScheme.tertiaryFixedDim,
+      "error":colorScheme.error,
+      "errorContainer":colorScheme.errorContainer,
+      "surface":colorScheme.surface,
+      "surfaceBright":colorScheme.surfaceBright,
+      "surfaceContainer":colorScheme.surfaceContainer,
+      "surfaceContainerHigh":colorScheme.surfaceContainerHigh,
+      "surfaceContainerHighest":colorScheme.surfaceContainerHighest,
+      "surfaceContainerLow":colorScheme.surfaceContainerLow,
+      "surfaceContainerLowest":colorScheme.surfaceContainerLowest,
+      "surfaceDim":colorScheme.surfaceDim,
+      "surfaceTint":colorScheme.surfaceTint,
+    };
+
     final logic = Get.put(UpdateLogic());
     return Scaffold(
         appBar: AppBar(
@@ -19,7 +47,23 @@ class UpdateManager extends StatelessWidget {
         ),
         body: Container(
           padding: const EdgeInsets.all(16),
-          child: const SizedBox(),
+          child: SingleChildScrollView(
+            child: Column(
+            children: colors.keys.map((key){
+                var color = colors[key];
+                return colorWidget(context,key,color!);
+            }).toList(),
+          ),
+          ),
         ));
+  }
+
+  Widget colorWidget(BuildContext context,String key,Color color){
+    return Row(
+      children: [
+        Text(key),
+        Expanded(child: Container(height: 20,color: color,margin: const EdgeInsets.only(left: 16,top: 8,bottom: 8),width: double.maxFinite,)),
+      ],
+    );
   }
 }
